bd59df3e3b6f0bd941c9c426f400f3f442ba9ede
Sachin Ganpule <>
2024-01-31 10:50:47 -0800

    Add error handling for out of range inputs

--- SudokuReader.cpp
+++ SudokuReader.cpp
@@ -4,20 +4,22 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int sudokuRead(fstream& is, vector<vector<int>> *square)
 {
-    char                cin;
+    string              cin;
     int                 cin_int;
     int                 returnVal = 0;
 
     is >> cin;
-    cin_int = stoi(&cin);
+    cin_int = stoi(cin);
     if ( (cin_int < 1) ||
          (cin_int > 9)    )
     {
        cin_int = 0;
+       cerr << "Found an entry (" << cin << ") not between 1-9... Replacing with 0" << endl;
     }
     cout << cin_int;
 
