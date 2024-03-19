eee9364c4bcc1ca372b82adbf5809f37a2a23db9
Sachin Ganpule <>
2024-03-19 11:29:50 -0700

    Adding two new inputs

--- gtest/SudokuReaderSolveTest.cpp
+++ gtest/SudokuReaderSolveTest.cpp
@@ -176,3 +176,31 @@ TEST_F(SudokuReaderSolveTest, MediumInput3) {
     status = m_sr.solve();
     ASSERT_EQ(status, SudokuReader::Solved);
 }
+
+TEST_F(SudokuReaderSolveTest, MediumInput4) {
+
+    m_inputFile = "../../inputs/medium_input_4.txt";
+    ASSERT_EQ(0, openInputFile(m_fin, m_inputFile));
+
+    // Read file
+    int status = m_sr.readFile(m_fin);
+    ASSERT_EQ(status, SudokuReader::NoError);
+
+    // Solve File
+    status = m_sr.solve();
+    ASSERT_EQ(status, SudokuReader::Solved);
+}
+
+TEST_F(SudokuReaderSolveTest, MediumInput5) {
+
+    m_inputFile = "../../inputs/medium_input_5.txt";
+    ASSERT_EQ(0, openInputFile(m_fin, m_inputFile));
+
+    // Read file
+    int status = m_sr.readFile(m_fin);
+    ASSERT_EQ(status, SudokuReader::NoError);
+
+    // Solve File
+    status = m_sr.solve();
+    ASSERT_EQ(status, SudokuReader::Solved);
+}
