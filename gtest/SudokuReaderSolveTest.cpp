45ff6ba15eb7c49082271c81249b61eb69ba687d
Sachin Ganpule <>
2024-04-29 23:14:38 -0700

    Add five master level inputs

--- gtest/SudokuReaderSolveTest.cpp
+++ gtest/SudokuReaderSolveTest.cpp
@@ -372,3 +372,88 @@ TEST_F(SudokuReaderSolveTest, ExpertInput5) {
     status = m_sr.solve();
     ASSERT_EQ(status, SudokuReader::Solved);
 }
+
+TEST_F(SudokuReaderSolveTest, ExpertInput6) {
+
+    m_inputFile = "../../inputs/expert_input_6.txt";
+    ASSERT_EQ(0, openInputFile(m_fin, m_inputFile));
+
+    // Read file
+    int status = m_sr.readFile(m_fin);
+    ASSERT_EQ(status, SudokuReader::NoError);
+
+    // Solve File
+    status = m_sr.solve();
+    ASSERT_EQ(status, SudokuReader::Solved);
+}
+
+TEST_F(SudokuReaderSolveTest, MasterInput1) {
+
+    m_inputFile = "../../inputs/master_input_1.txt";
+    ASSERT_EQ(0, openInputFile(m_fin, m_inputFile));
+
+    // Read file
+    int status = m_sr.readFile(m_fin);
+    ASSERT_EQ(status, SudokuReader::NoError);
+
+    // Solve File
+    status = m_sr.solve();
+    ASSERT_EQ(status, SudokuReader::Solved);
+}
+
+TEST_F(SudokuReaderSolveTest, MasterInput2) {
+
+    m_inputFile = "../../inputs/master_input_2.txt";
+    ASSERT_EQ(0, openInputFile(m_fin, m_inputFile));
+
+    // Read file
+    int status = m_sr.readFile(m_fin);
+    ASSERT_EQ(status, SudokuReader::NoError);
+
+    // Solve File
+    status = m_sr.solve();
+    ASSERT_EQ(status, SudokuReader::Solved);
+}
+
+TEST_F(SudokuReaderSolveTest, MasterInput3) {
+
+    m_inputFile = "../../inputs/master_input_3.txt";
+    ASSERT_EQ(0, openInputFile(m_fin, m_inputFile));
+
+    // Read file
+    int status = m_sr.readFile(m_fin);
+    ASSERT_EQ(status, SudokuReader::NoError);
+
+    // Solve File
+    status = m_sr.solve();
+    ASSERT_EQ(status, SudokuReader::Solved);
+}
+
+TEST_F(SudokuReaderSolveTest, MasterInput4) {
+
+    m_inputFile = "../../inputs/master_input_4.txt";
+    ASSERT_EQ(0, openInputFile(m_fin, m_inputFile));
+
+    // Read file
+    int status = m_sr.readFile(m_fin);
+    ASSERT_EQ(status, SudokuReader::NoError);
+
+    // Solve File
+    status = m_sr.solve();
+    ASSERT_EQ(status, SudokuReader::Solved);
+}
+
+TEST_F(SudokuReaderSolveTest, MasterInput5) {
+
+    m_inputFile = "../../inputs/master_input_5.txt";
+    ASSERT_EQ(0, openInputFile(m_fin, m_inputFile));
+
+    // Read file
+    int status = m_sr.readFile(m_fin);
+    ASSERT_EQ(status, SudokuReader::NoError);
+
+    // Solve File
+    status = m_sr.solve();
+    ASSERT_EQ(status, SudokuReader::Solved);
+}
+
